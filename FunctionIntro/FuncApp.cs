9935b14d3d55dec27e0c22e397feafc8b87d4bad
Matej Dolecek <1179@adc.itgymnazium.cz>
2023-12-05 09:24:57 +0100

    1179_cykly

---
+++ FunctionIntro/FuncApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionIntro;
+internal class FuncApp
+{
+    public void Run()
+    {
+        string input = Console.ReadLine();
+        int number = SafelyConvertToInt(input);
+        int a = int.Parse(input);
+        int b = Convert.ToInt32(input);
+    }
+
+    public int SafelyConvertToInt(string x)
+    {
+        if (int.TryParse(x, out int resoult))
+        {
+            return resoult;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
---
+++ FunctionIntro/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FunctionIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncApp app = new FuncApp();
+            app.Run();
+        }
+    }
+}
